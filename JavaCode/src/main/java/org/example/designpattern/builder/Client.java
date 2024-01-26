f8581e020d95143bd46675429bc350e135272ab1
Sk Alamin <91690390+alaminskofficial@users.noreply.github.com>
2024-01-26 18:56:38 +0000

    builder code added

---
+++ JavaCode/src/main/java/org/example/designpattern/builder/Client.java
@@ -0,0 +1,15 @@
+package org.example.designpattern.builder;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Student student = Student.createBuilder()
+                .setAge(26)
+                .setName("Alamin")
+                .setGender(Gender.MALE)
+//                .setPsp(91.99)
+                .setUniversityName("Jadavpur University")
+                .setPhoneNumber("1123456789")
+                .build();
+    }
+
+}
---
+++ JavaCode/src/main/java/org/example/designpattern/builder/Gender.java
@@ -0,0 +1,6 @@
+package org.example.designpattern.builder;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+}
